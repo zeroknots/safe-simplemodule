aba1f1bb68cdc1a3d76eb76f7bb2a8c86fbb1d5d
zeroknots <zeroknots@protonmail.com>
2023-04-24 08:40:30 +0700

    adding module

--- src/SafeModuleSimple.sol
+++ src/SafeModuleSimple.sol
@@ -2,4 +2,16 @@ pragma solidity 0.8.19;
 
 import {Module} from "zodiac/core/Module.sol";
 
-contract SafeModuleSimple is Module {}
+import "forge-std/console2.sol";
+
+contract SafeModuleSimple is Module {
+    function fooCall() external {
+        console2.log("fooCall");
+        return 1337;
+    }
+
+    function fooDelegatecall() external {
+        console2.log("fooDelegatecall");
+        return 1338;
+    }
+}
--- test/SafeModule.t.sol
+++ test/SafeModule.t.sol
@@ -3,13 +3,19 @@ pragma solidity 0.8.19;
 import "safe-tools/SafeTestTools.sol";
 import "forge-std/Test.sol";
 
+import "../src/SafeModuleSimple.sol";
+
 contract SafeModuleTest is Test, SafeTestTools {
     using SafeTestLib for SafeInstance;
 
     SafeInstance safeInstance;
 
+    SafeModuleSimple simpleModule;
+
     function setUp() public {
         safeInstance = _setupSafe();
+
+        simpleModule = new SafeModuleSimple();
     }
 
     function testSendSimpleTransaction() public {
@@ -20,14 +26,14 @@ contract SafeModuleTest is Test, SafeTestTools {
     }
 
     function testEnableModule() public {
-        address module = address(0x6969);
+        address module = address(simpleModule);
 
         safeInstance.enableModule(module);
         assertTrue(safeInstance.safe.isModuleEnabled(module));
     }
 
     function testDisableModule() public {
-        address module = address(0x6969);
+        address module = address(simpleModule);
 
         safeInstance.enableModule(module);
         assertTrue(safeInstance.safe.isModuleEnabled(module));
@@ -35,38 +41,40 @@ contract SafeModuleTest is Test, SafeTestTools {
         assertFalse(safeInstance.safe.isModuleEnabled(module));
     }
 
-    function testSignTransaction() public {
+    // function testSignTransaction() public {
+    //     address alice = address(0x1234);
+    //     uint256[] memory ownerPKs = new uint256[](1);
+    //     ownerPKs[0] = 12345;
+    //     SafeInstance memory instance = _setupSafe({ownerPKs: ownerPKs, threshold: 1, initialBalance: 1 ether});
+    //     (uint8 v, bytes32 r, bytes32 s) = instance.signTransaction(
+    //         ownerPKs[0], alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), address(0)
+    //     );
+    //     bytes memory signature = abi.encodePacked(r, s, v);
+    //     instance.safe.execTransaction(
+    //         alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), signature
+    //     );
+    //     assertEq(alice.balance, 0.5 ether);
+    // }
+
+    function testExecModule() public {
+        bytes memory functionOnModule = abi.encodeWithSelector(SafeModuleSimple.fooCall.selector);
+        bytes memory execModule = abi.encodeWithSelector(
+            ModuleManager.execTransactionFromModule.selector, address(simpleModule), 0, functionOnModule
+        );
+
+        address alice = address(0x1234);
         uint256[] memory ownerPKs = new uint256[](1);
         ownerPKs[0] = 12345;
-        SafeInstance memory instance = _setupSafe({
-            ownerPKs: ownerPKs,
-            threshold: 1,
-            initialBalance: 1 ether
-        });
+        SafeInstance memory instance = _setupSafe({ownerPKs: ownerPKs, threshold: 1, initialBalance: 1 ether});
+
+        address module = address(simpleModule);
+        instance.enableModule(module);
         (uint8 v, bytes32 r, bytes32 s) = instance.signTransaction(
-            ownerPKs[0],
-            alice,
-            0.5 ether,
-            "",
-            Enum.Operation.Call,
-            0,
-            0,
-            0,
-            address(0),
-            address(0)
+            ownerPKs[0], alice, 0.5 ether, execModule, Enum.Operation.Call, 0, 0, 0, address(0), module
         );
         bytes memory signature = abi.encodePacked(r, s, v);
         instance.safe.execTransaction(
-            alice,
-            0.5 ether,
-            "",
-            Enum.Operation.Call,
-            0,
-            0,
-            0,
-            address(0),
-            payable(address(0)),
-            signature
+            alice, 0.5 ether, execModule, Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), signature
         );
         assertEq(alice.balance, 0.5 ether);
     }
