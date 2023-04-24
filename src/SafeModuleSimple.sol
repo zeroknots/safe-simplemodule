0ae82c7e84a25278f17a372eca0c5429faa0fee8
zeroknots <zeroknots@protonmail.com>
2023-04-24 08:47:58 +0700

    path mess

--- src/SafeModuleSimple.sol
+++ src/SafeModuleSimple.sol
@@ -5,12 +5,14 @@ import {Module} from "zodiac/core/Module.sol";
 import "forge-std/console2.sol";
 
 contract SafeModuleSimple is Module {
-    function fooCall() external {
+    function setUp(bytes memory initParams) override public {}
+
+    function fooCall() external returns (uint256) {
         console2.log("fooCall");
         return 1337;
     }
 
-    function fooDelegatecall() external {
+    function fooDelegatecall() external returns (uint256) {
         console2.log("fooDelegatecall");
         return 1338;
     }
--- test/SafeModule.t.sol
+++ test/SafeModule.t.sol
@@ -41,20 +41,20 @@ contract SafeModuleTest is Test, SafeTestTools {
         assertFalse(safeInstance.safe.isModuleEnabled(module));
     }
 
-    // function testSignTransaction() public {
-    //     address alice = address(0x1234);
-    //     uint256[] memory ownerPKs = new uint256[](1);
-    //     ownerPKs[0] = 12345;
-    //     SafeInstance memory instance = _setupSafe({ownerPKs: ownerPKs, threshold: 1, initialBalance: 1 ether});
-    //     (uint8 v, bytes32 r, bytes32 s) = instance.signTransaction(
-    //         ownerPKs[0], alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), address(0)
-    //     );
-    //     bytes memory signature = abi.encodePacked(r, s, v);
-    //     instance.safe.execTransaction(
-    //         alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), signature
-    //     );
-    //     assertEq(alice.balance, 0.5 ether);
-    // }
+    function testSignTransaction() public {
+        address alice = address(0x1234);
+        uint256[] memory ownerPKs = new uint256[](1);
+        ownerPKs[0] = 12345;
+        SafeInstance memory instance = _setupSafe({ownerPKs: ownerPKs, threshold: 1, initialBalance: 1 ether});
+        (uint8 v, bytes32 r, bytes32 s) = instance.signTransaction(
+            ownerPKs[0], alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), address(0)
+        );
+        bytes memory signature = abi.encodePacked(r, s, v);
+        instance.safe.execTransaction(
+            alice, 0.5 ether, "", Enum.Operation.Call, 0, 0, 0, address(0), payable(address(0)), signature
+        );
+        assertEq(alice.balance, 0.5 ether);
+    }
 
     function testExecModule() public {
         bytes memory functionOnModule = abi.encodeWithSelector(SafeModuleSimple.fooCall.selector);
