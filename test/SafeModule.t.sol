d1e6d8550fb1b59edd54da39afe4d965d38c5b13
zeroknots <zeroknots@protonmail.com>
2023-04-24 08:11:44 +0700

    add sign / exec Tx to test rig

--- test/SafeModule.t.sol
+++ test/SafeModule.t.sol
@@ -34,4 +34,40 @@ contract SafeModuleTest is Test, SafeTestTools {
         safeInstance.disableModule(module);
         assertFalse(safeInstance.safe.isModuleEnabled(module));
     }
+
+    function testSignTransaction() public {
+        uint256[] memory ownerPKs = new uint256[](1);
+        ownerPKs[0] = 12345;
+        SafeInstance memory instance = _setupSafe({
+            ownerPKs: ownerPKs,
+            threshold: 1,
+            initialBalance: 1 ether
+        });
+        (uint8 v, bytes32 r, bytes32 s) = instance.signTransaction(
+            ownerPKs[0],
+            alice,
+            0.5 ether,
+            "",
+            Enum.Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            address(0)
+        );
+        bytes memory signature = abi.encodePacked(r, s, v);
+        instance.safe.execTransaction(
+            alice,
+            0.5 ether,
+            "",
+            Enum.Operation.Call,
+            0,
+            0,
+            0,
+            address(0),
+            payable(address(0)),
+            signature
+        );
+        assertEq(alice.balance, 0.5 ether);
+    }
 }
